b455c15c8c731c9e2f2ca473e45f76fbfa211629
david <david@spektrakel.de>
2017-07-26 09:38:06 +0200

    feat: it still works w/ typescript 2.2.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+
+  public ngOnInit() {
+
+    Observable.of('foo')
+      .subscribe();
+
+  }
 }
